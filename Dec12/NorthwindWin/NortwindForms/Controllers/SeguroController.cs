85194f635db3763bdee47400602c1060abbcad6d
gcvalderrama@hotmail.com <gcvalderrama@hotmail.com>
2015-12-12 18:57:08 -0500

    add role manger

---
+++ Dec12/NorthwindWin/NortwindForms/Controllers/SeguroController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NortwindForms.Controllers
+{
+    [Authorize(Roles = "Managers")]
+    public class SeguroController : Controller
+    {
+        // GET: Seguro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
