00784b22b4e376327fc6c565178561e1384b1e28
gcvalderrama@hotmail.com <gcvalderrama@hotmail.com>
2015-12-12 17:05:29 -0500

    add

---
+++ Dec12/NorthwindWin/NorthwindWin/Controllers/RegionesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindWin.Controllers
+{
+    [Authorize]
+    public class RegionesController : Controller
+    {
+        // GET: Regiones
+        public ActionResult Index()
+        {
+            var context = new Models.NorthwindEntities();  
+
+            var regiones = context.Region.ToList();
+
+            return View(regiones);
+        }
+    }
+}
