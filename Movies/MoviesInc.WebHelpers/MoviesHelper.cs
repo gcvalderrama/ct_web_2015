2b52a5dd5712fa99e374fa1cf355a84973242499
gcvalderrama@hotmail.com <gcvalderrama@hotmail.com>
2016-01-30 18:57:13 -0500

    Add Ajax

---
+++ Movies/MoviesInc.WebHelpers/MoviesHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html; 
+namespace MoviesInc.WebHelpers
+{
+        
+    public static class MoviesHelper
+    {
+        public static MvcHtmlString 
+            MoviesTextbox(this HtmlHelper htmlHelper, string Name, string Value =  "enter data")
+        {  
+
+            return htmlHelper.TextBox(Name, Value ,
+                new { @data_role =  "busqueda" }
+            );
+        }
+    }
+}
---
+++ Movies/MoviesSite/Controllers/SimpleController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesSite.Controllers
+{
+    public class SimpleController : Controller
+    {
+        // GET: Simple
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string FirstName)
+        {
+            //Session["FirstName"] = FirstName;
+            //return RedirectToAction("Index3");
+
+            
+
+            TempData["FirstName"] = FirstName;
+            return this.RedirectToAction("Index4");
+
+        }
+        public ActionResult Index2(string mensaje)
+        {
+            this.ViewBag.Mensaje = mensaje;
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            this.ViewBag.Mensaje = Session["FirstName"];
+            return View();
+        }
+        public ActionResult Index4()
+        {
+            this.ViewBag.Mensaje = TempData["FirstName"];
+            return View();
+        }
+
+        public ActionResult Index5()
+        {            
+            return View();
+        }
+
+    }
+}
